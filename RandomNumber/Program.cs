89106f1497f5602cf754eb3217f3d878db68ebeb
Küllike Reineberg <kullike.reineberg@tktk.ee>
2024-09-12 19:25:40 +0300

    add week4 code

---
+++ RandomNumber/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Net.Http.Headers;
+
+Random rnd = new Random();
+
+int myRandomNum;
+
+int randomSum = 0;
+
+for(int i = 0; i < 10; i++)
+{
+    myRandomNum = rnd.Next(0, 11);
+    randomSum = randomSum + myRandomNum;
+    Console.WriteLine($"My random number is: {myRandomNum}");
+}
+Console.WriteLine($"Random sum total: {randomSum}");
